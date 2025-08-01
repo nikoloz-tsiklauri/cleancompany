6abbfbbec309b090c8c67cd89ed077c3a10d17fa
lashaTech <lashatsiklauri77@gmail.com>
2025-08-02 01:36:51 +0400

    first commit

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post('/send-email', async (req, res) => {
     res.status(500).send({ success: false, message: 'Failed to send email' });
   }
 });
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
